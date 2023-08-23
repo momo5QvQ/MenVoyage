83baa0c1e6bfb971774bde064b7e3a85655529b7
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-08-23 11:45:37 +0000

    [add]comment-cre-des

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,13 +1,23 @@
 class Public::CommentsController < ApplicationController
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    comment = current_customer.comments.new(comment_params)
-    comment.recipe_id = recipe.id
-    comment.save
-    redirect_to recipe_path(recipe)
-  
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = current_customer.comments.new(comment_params)
+    @comment.recipe_id = @recipe.id
+    @comment.save
+
   end
 
   def destroy
+    # Comment.find(id: params[:id], recipe_id: params[:recipe_id]).destroy
+    # @recipe = Recipe.find(params[:recipe_id])
+    comment = Comment.find_by(id: params[:id], recipe_id: params[:recipe_id])
+    comment.destroy if comment
+    @recipe = Recipe.find(params[:recipe_id])
   end
+
+  private
+
+   def comment_params
+     params.require(:comment).permit(:comment)
+   end
 end
